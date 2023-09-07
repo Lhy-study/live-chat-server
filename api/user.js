b48f1a13a03521d73799db87ff2a61b1a2e7ae0a
LiuHouYu <Lhy17329785877@outlook.com>
2023-09-07 22:34:04 +0800

    配置了有关user的相关路由和api

--- api/user.js
+++ api/user.js
@@ -1,12 +1,12 @@
 const prisma = require("../prisma");
-// map.set(name1,'username');
 
-用户注册
-function createUser({username,username}){
-    return new Promise((resolve,reject)=>{
+
+//用户注册
+function createUser({ username, password }) {
+    return new Promise(async (resolve, reject) => {
         try {
-            prisma.user.create({
-                data:{
+            await prisma.user.create({
+                data: {
                     username,
                     password
                 }
@@ -18,8 +18,36 @@ function createUser({username,username}){
     })
 }
 
-修改用户性别
-function updateGender({ uid, value, type }) {
+//用户登录
+function login({ username, password }) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const result = await prisma.user.findFirst({
+                where: {
+                    username,
+                }
+            });
+            if (!result.username) {
+                throw new Error("找不到该用户")
+            }
+            if (result.password !== password) {
+                throw new Error("密码错误")
+            }
+            const data = await prisma.user.findFirst({
+                where: {
+                    username,
+                },
+            })
+            delete data.password //删除密码属性
+            resolve(data)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+//修改用户性别
+function update({ uid, value, type }) {
     const data = {};
     data[`${type}`] = value
     return new Promise(async (resolve, reject) => {
@@ -35,6 +63,24 @@ function updateGender({ uid, value, type }) {
             reject(error)
         }
     })
+}
+
+//为进行有关user的某些功能所需额外的操作
+
+// 用户之前注册查找是否重名
+function findUser(username) {
+    return prisma.user.findMany({
+        where: {
+            username,
+        }
+    });
+}
+
+module.exports = {
+    createUser,
+    login,
+    update,
+    findUser
 }
 
 
--- route/user.js
+++ route/user.js
@@ -1,3 +1,103 @@
-const router=require("express").Router();
+const router = require("express").Router();
+const prisma = require("../prisma");
+const { createUser, update, login, findUser } = require("../api/user");
+const jwt = require("jsonwebtoken");
+const { secretky } = require("../global");
 
-module.exports=router
+//用户注册 1.首先要格式校验
+router.post("/reg", async (request, response) => {
+    const { username, password } = request.body;
+    const result = await findUser(username);
+    if (/^[\u4e00-\u9fa5a-zA-Z0-9]{1,7}$/.test(username) && /^(?=.*[0-9])(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/.test(password)) {
+        response.send({
+            code: 400,
+            msg: "请检查用户名或者密码是否符合规范",
+        });
+    }else if (result.length) {//代表已经有用户了
+        response.send({
+            code: 405,
+            msg: "用户已经存在,请换个用户名"
+        })
+    } else {
+        createUser({username,password})
+            .then(()=>{
+                response.send({
+                    code:201,
+                    msg:"注册成功，请返回登录页登录"
+                })
+            })
+            .catch(()=>{
+                response.send({
+                    code:500,
+                    msg:"注册失败，请稍后重试"
+                })
+            })
+            .finally(()=>{
+                prisma.$disconnect();
+            })
+    }
+})
+
+//用户登录
+router.post("/login", (request, response) => {
+    const { username, password } = request.body
+    login({ username, password })
+        .then((data) => {
+            const token = jwt.sign(data, secretky, { expiresIn: "7d" });
+            response.send({
+                code: 200,
+                msg: "登录成功",
+                data,
+                token,
+            })
+        })
+        .catch((e) => {
+            console.log(e);
+            response.send({
+                code: 400,
+                msg: "e"
+            });
+        })
+        .finally(() => {
+            prisma.$disconnect();
+        })
+});
+
+//持久化登录
+router.get("/isLogin", (request, response) => {
+    const token = request.headers['authorization'];
+
+    if (token) {
+        jwt.verify(token, secretky, (err, user) => {
+            if (err) {
+                response.send({
+                    code: 401,
+                    msg: "您没有该权限",
+                    e: err
+                });
+            } else {
+                response.send({
+                    code: 200,
+                    msg: "登录成功",
+                    data: user
+                });
+            }
+        })
+    } else {
+        response.send({
+            code: 404,
+            data: { uid: NaN },
+            msg: "登录失败"
+        });
+    }
+});
+
+//用户修改信息(不包括密码、头像)
+router.post("/update/:type", (request, response) => {
+    const { uid, value } = request.body;
+    const type = request.params.type;
+    update({ uid, value, type })
+        .then
+});
+
+module.exports = router
